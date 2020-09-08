b1eeb1681696d74618821ddcc242b12735206c2e
איליה טובבין <tovbin_i@mac.org.il>
2020-09-08 21:42:27 +0300

    Add project files.

---
+++ GetFreeSpace/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetFreeSpace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SpaceChecker CheckNow = new SpaceChecker();
+            CheckNow.Location = args[0];
+            try
+            {
+                string x = CheckNow.Get();
+                Console.WriteLine(x.ToString());
+            }
+            catch (Exception r) { Console.WriteLine("Error getting free space: " + r.Message); }
+            
+        }
+    }
+}
---
+++ GetFreeSpace/SpaceChecker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace GetFreeSpace
+{
+    class SpaceChecker
+    {
+
+        [DllImport("kernel32")]
+        public static extern int GetDiskFreeSpaceEx(
+        string lpDirectoryName,
+        ref long lpFreeBytesAvailable,
+        ref long lpTotalNumberOfBytes,
+        ref long lpTotalNumberOfFreeBytes
+        );
+
+        public string Location { get; set; }
+
+        public string Get()
+        {
+            long FreeBytesAvailable = 0;
+            long TotalNumberOfBytes = 0;
+            long TotalNumberOfFreeBytes = 0;
+
+            int bRC = GetDiskFreeSpaceEx(Location, ref FreeBytesAvailable, ref TotalNumberOfBytes, ref TotalNumberOfFreeBytes);
+            long FreeGB = FreeBytesAvailable / 1024 / 1024 / 1024;
+            long TotalGB = TotalNumberOfBytes / 1024 / 1024 / 1024;
+            float FreePercentage = ((float)FreeGB / (float)TotalGB) * 100;
+            return FreePercentage.ToString("0.00");
+        }
+    }
+}
